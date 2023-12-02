503cc99ad7cd2f2cfc03366f7b16d40a697c53c9
Chandrapriya2613 <123033825+Chandrapriya2613@users.noreply.github.com>
2023-12-01 23:24:12 -0600

    Uploading app file

---
+++ App.jsx
@@ -0,0 +1,26 @@
+import Home from './Home';
+import Login from './Login';
+import Register from './Register';
+import Forgot from './forgot';
+import PasswordResetpage from './PasswordResetpage';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
+function App() {
+
+  return (
+    <div style={{marginTop : '-3.5rem'}}>
+      <BrowserRouter >
+        <Routes>
+          <Route path="/" element ={<Login/>} />
+          <Route path="/register" element ={<Register/>} />
+          <Route path="/login" element ={<Login/>} />
+          <Route path="/home" element ={<Home/>} />
+          <Route path="/forgot" element ={<Forgot/>} />
+          <Route path="/PasswordResetpage" element ={<PasswordResetpage/>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
