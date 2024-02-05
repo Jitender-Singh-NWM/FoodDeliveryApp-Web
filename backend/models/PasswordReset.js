57f0214f0ba91d27895afc619c14f7f3e95a7cf9
Jitender-Singh-NWM <122651570+Jitender-Singh-NWM@users.noreply.github.com>
2024-02-05 09:26:15 -0600

    Folder Cleanup

---
+++ backend/models/PasswordReset.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const passwordResetSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'FormDataModel',
+    required: true,
+  },
+  token: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    expires: 3600, // Set the expiration time for the document (1 hour in seconds)
+  },
+});
+
+const PasswordReset = mongoose.model('PasswordReset', passwordResetSchema);
+
+module.exports = PasswordReset;
