bfb080ff142dedc685699a028085624a31e95c4e
Jitender-Singh-NWM <122651570+Jitender-Singh-NWM@users.noreply.github.com>
2024-02-18 22:06:21 -0600

    components

--- frontend/src/components/CometChatUI.tsx
+++ frontend/src/components/CometChatUI.tsx
@@ -61,7 +61,7 @@ export function CometChatUI() {
     } else {
     	setIsMobileView(false);
     }
-  }; 
+  };   
                                          
   useEffect(() => {
     resizeWindow();
