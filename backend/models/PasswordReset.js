3083da1db5f4c1b2dfa8acbc003dd68ed0a41f03
Ashok kumar Ganji <g.ashokkumar2k22@gmail.com>
2024-04-21 23:20:40 -0500

    added new functionality

--- backend/models/PasswordReset.js
+++ backend/models/PasswordReset.js
@@ -10,6 +10,10 @@ const passwordResetSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  token: {
+    type: String,
+    required: true,
+  },
   createdAt: {
     type: Date,
     default: Date.now,
